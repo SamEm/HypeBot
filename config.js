dcbf910cd20ac9f510433a5ea6b3b87044e36b9d
Logiz <l.samemami@gmail.com>
2017-05-04 16:41:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,51 +1,16 @@
 var config = {
 
   botLogChannel: '282959338887315459',
-  botSpam: '307324599861903363',
-  
+
   modRole: '258357921505738752',
   hypeQueenRole: '258333211300462612',
   discordDevRole: '200719965437820928',
   hypeAlchRole: '282233528089968643',
 
-  denmarkRole: '245619528224997379',
-  canadaRole: '244230794413998080',
-  brazilRole: '244231073498791937',
-  australiaRole: '248856061203841045',
-  mountainRole: '244231892474396672',
-  belgiumRole: '268464326820495360',
-  serbiaRole: '259438351692726273',
-  irelandRole: '244235647706071040',
-  spainRole: '255790548466532352',
-  finlandRole: '244234866818940928',
-  germanyRole: '244231069413408769',
-  centralRole: '265937810731565056',
-  russiaRole: '296722342053150721',
-  polandRole: '258732939616059392',
-  norwayRole: '244234761789243393',
-  czechRole: '245965740107235338',
-  ukRole: '244231223528914946',
-  lithuaniaRole: '244236784488284160',
-  franceRole: '245965402230751232',
-  swedenRole: '248188867553787905',
-  NZRole: '257779110657392640',
-  pacificRole: '244231963974565888',
-  portugalRole: '246071476690747394',
-  mexicoRole: '279689382489751552',
-  turkeyRole: '244231544451891200',
-  sloveniaRole: '244235514864074752',
-  estoniaRole: '276396950608150528',
-  easternRole: '244231750534823937',
-  netherlandsRole: '245304094472929280',
-  italyRole: '258880784713121794',
-  switzerlandRole: '247775856515219458',
-  israelRole: '291456001478098945',
-  scotlandRole: '296356252542566407',
-  southARole: '298617952813580289',
-  asiaRole: '298624295498809344',
-
 
   everybodyRole: 'everybody', // Fake role for commands everyone can use
+  allChannels: 'allChannels', //fake channel
+  botSpam: '307324599861903363', //bot spam channel
 
   botID: '290910991175254016',
   HSserverID: '200661830648070145',
