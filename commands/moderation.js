d2174dbc61b268a2b16ee7b79ac70c15928ecf51
Logiz <l.samemami@gmail.com>
2017-06-09 17:15:51 +0200

    New !addRole command

--- commands/moderation.js
+++ commands/moderation.js
@@ -1,11 +1,17 @@
 "use strict";
 const config = require("../config");
-let utils = require('../utils');
+const utils = require('../utils');
 
 let moderation = {
-  pattern: /!restart|!ping|!roles/i,
+  pattern: /!restart|!ping|!addrole/i,
   execute: function(bot, channelID, userTag, userID, command, msg) {
     switch (command.toLowerCase()) {
+      case "!roles":
+        let roles = msg.member.guild.roles;
+        let rolesList = roles.map(function(role){
+          return role.name + ": '" + role.id + "'"; });
+        bot.createMessage(channelID, "```js\n" + rolesList.join(",\n") + "```").catch((err) => {console.log(err);});
+        break;
       case "!restart":
         console.log("Restarting");
         bot.deleteMessage(channelID, msg.id).then(() => {
@@ -17,11 +23,60 @@ let moderation = {
         bot.createMessage(channelID, "Pong!");
         break;
 
-      case "!roles":
-        let roles = msg.member.guild.roles;
-        let rolesList = roles.map(function(role){
-          return role.name + ": '" + role.id + "'"; });
-        bot.createMessage(config.botLogChannel, "```js\n" + rolesList.join(",\n") + "```").catch((err) => {console.log(err);});
+      case "!addrole":
+        let msgSplit = msg.content.split(' ');
+        msgSplit.shift();
+        msgSplit.join('\n');
+        let userList = msgSplit[0].split('\n');
+        let roleID = userList.shift();
+
+        if(!roleID) {
+          bot.createMessage(channelID, "EHM, I believe you forgot something! (Role ID and/or users to add)");
+          return;
+        }
+
+        let checkRoleValid = msg.channel.guild.roles.get(roleID);
+        if(checkRoleValid === -1) {
+          bot.createMessage(channelID, "Whoops, doesn't look like that role exists. Might want to recheck that!");
+          return;
+        }
+
+        //check if role exists
+        //assign all roles to userIDs
+        //drop failed roles into array
+
+        bot.createMessage(channelID, "Working...");
+
+        let usersHaveRole = [];
+        let nullUsers = [];
+
+        let addRoles = userList.reduce((previousID, userID) => {
+          return previousID
+          .then(utils.delay(1500))
+          .then(thisID => {
+            let getUser = msg.channel.guild.members.get(userID);
+            if(!getUser) {
+              nullUsers.push(userID);
+            } else {
+              let checkUserRoles = getUser.roles.indexOf(roleID);
+              if(checkUserRoles > -1) {
+                usersHaveRole.push(userID);
+              } else {
+                getUser.roles.push(roleID);
+                bot.editGuildMember(msg.channel.guild.id, userID, {
+                  roles: getUser.roles
+                }).catch((err) => {
+                  //console.log(err);
+                });
+              }
+            }
+            return doneMsg (bot, channelID, usersHaveRole, nullUsers);
+          });
+        }, Promise.resolve([]));
+
+        addRoles.then(result => {
+          bot.createMessage(channelID, "Done." + result).catch((err) => {console.log("cookies\n" + err);})
+        });
         break;
 
     }
@@ -30,10 +85,29 @@ let moderation = {
     config.modRole,
     config.hypeQueenRole,
     config.discordDevRole,
-    config.hypeAlchRole
+    config.hypeAlchRole,
+    config.everybodyRole
     ],
   channels: [
     config.allChannels
   ]
+}
+
+function doneMsg (bot, channelID, usersHaveRole, nullUsers) {
+
+  let hasRoleTxt;
+  let missingUsrTxt;
+
+  if(usersHaveRole.length !== 0) {
+    hasRoleTxt = "\nThese users already had the role:\n```\n" + usersHaveRole.join('\n') + "```";
+  }else {
+    hasRoleTxt = "";
+  }
+  if(nullUsers.length !== 0) {
+    missingUsrTxt = "\nCould not find these users:\n```\n" + nullUsers.join('\n') + "```";
+  } else {
+    missingUsrTxt = "";
+  }
+  return hasRoleTxt + missingUsrTxt;
 }
 module.exports = moderation;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Eris = require('eris');
 const config = require('./config');
 const commandList = require('./commandList')();
 const token = require('./token');
+const dateFormat = require('dateformat');
+const fs = require('fs');
 
 let bot = new Eris(token.botToken, {
   getAllUsers: true
@@ -46,33 +48,36 @@ function correctChannelIsBeingUsed(guild, command) {
 }
 
 bot.on('messageCreate', (msg) => {
-  var messageSplit = msg.content.split(' ');
-  var command = messageSplit.shift();
 
-  var channelID = msg.channel.id;
-  var userTag = msg.author.username + "#" + msg.author.discriminator;
-  var userID = msg.author.id;
+  if(!!msg.channel.guild && msg.channel.guild.id === config.HSserverID) {
+    var messageSplit = msg.content.split(' ');
+    var command = messageSplit.shift();
 
-  if(userID === "84815422746005504" && command.toLowerCase() === "!log") {
-    let now = new Date();
-    let thisCycle = dateFormat(now, "UTC:mm-dd-yyyy-HH-MM");
-    let bufferString = fs.readFileSync('./logs/hblog.log');
-    bot.getDMChannel(userID).then((thisDM) => {
-      bot.createMessage(thisDM.id, null, {file: bufferString, name: "hb.log"}).catch((error) => {console.log(error);});
-    });
-  }
+    var channelID = msg.channel.id;
+    var userTag = msg.author.username + "#" + msg.author.discriminator;
+    var userID = msg.author.id;
 
-  if(command.match(/^!/)) {
-    let matchingCommand = commandList.find(command);
-    //check for roles
-    if(userHasAuthorityForCommand(msg.member, matchingCommand)) {
-      if(correctChannelIsBeingUsed(msg.channel, matchingCommand) !== true) {
-        return;
+    if(userID === "84815422746005504" && command.toLowerCase() === "!log") {
+      let now = new Date();
+      let thisCycle = dateFormat(now, "UTC:mm-dd-yyyy-HH-MM");
+      let bufferString = fs.readFileSync('./logs/hblog.log');
+      bot.getDMChannel(userID).then((thisDM) => {
+        bot.createMessage(thisDM.id, null, {file: bufferString, name: "hb.log"}).catch((error) => {console.log(error);});
+      });
+    }
+
+    if(command.match(/^!/)) {
+      let matchingCommand = commandList.find(command);
+      //check for roles
+      if(userHasAuthorityForCommand(msg.member, matchingCommand)) {
+        if(correctChannelIsBeingUsed(msg.channel, matchingCommand) !== true) {
+          return;
+        }
+
+        matchingCommand.execute(bot, channelID, userTag, userID, command, msg);
+      }else {
+        // Prepared for later functionality
       }
-
-      matchingCommand.execute(bot, channelID, userTag, userID, command, msg);
-    }else {
-      // Prepared for later functionality
     }
   }
 });
