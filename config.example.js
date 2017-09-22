81847d402e488afeaf62b8c5d71b5c9a1009ed65
Logiz <l.samemami@gmail.com>
2017-09-22 14:22:18 +0200

    example configs

---
+++ config.example.js
@@ -0,0 +1,22 @@
+var config = {
+
+//Channel the bot logs information to
+  botLogChannel: '282959338887315459',
+
+//List of roles that have access to commands in moderation.js
+  modRole: '258357921505738752',
+  hypeQueenRole: '258333211300462612',
+  discordDevRole: '200719965437820928',
+  hypeAlchRole: '282233528089968643',
+
+  everybodyRole: 'everybody', // Fake role for commands everyone can use
+  allChannels: 'allChannels', //fake channel
+
+  //specific channel that can only be used for bot commands
+  botSpam: '307324599861903363', //bot spam channel
+
+  botID: '290910991175254016',
+  HSserverID: '200661830648070145',
+  delayInMS: 5000
+}
+module.exports = config;
---
+++ ids.example.js
@@ -0,0 +1,6 @@
+var ids = {
+  norway: '244234761789243393'
+//rolename: 'roleID'
+}
+
+module.exports = ids;
